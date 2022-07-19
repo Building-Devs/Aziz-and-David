fcd944f7ebe1457594563145bab077d4902ef791
David Ampofo Quarcoo <106585810+Beloved21@users.noreply.github.com>
2022-07-19 22:19:58 +0000

    Update Assignment 2.py

--- Assignment 2.py
+++ Assignment 2.py
@@ -1,6 +1,6 @@
-while True:
-    n = int(input("Enter row: "))
-    for i in range(1,n+1):
+while True:                 
+    n = int(input("Enter row: "))                  # This specifies the numbers of rows to print
+    for i in range(1,n+1):                         # It iterates the given range and executes the following if statements provided the conditions are true
         if i ==1:
             print("*")
         if i >1:
