b463c4fafd9ce55a9d516286a24de4e4d5af850a
David Ampofo Quarcoo <106585810+Beloved21@users.noreply.github.com>
2022-07-19 22:25:07 +0000

    Update Assignment 3.py

--- Assignment 3.py
+++ Assignment 3.py
@@ -1,9 +1,9 @@
 while True:
-    n = int(input("Enter number of row: ")) 
-    for i in range(n):
-        for j in range(n - i - 1):
+    n = int(input("Enter number of row: "))            
+    for i in range(n):                       # It iterates each row in the given range
+        for j in range(n - i - 1):           # It iterates each column in the specified range and print spaces depending on the given range
             print(' ', end='')
-        for j in range(2 * i+2):
+        for j in range(2 * i+2):                 # It iterates the given range and executes the following if statements provided the conditions are true
             if j == 0 :
                 print('*', end='')
             elif  j == 2 * i:
